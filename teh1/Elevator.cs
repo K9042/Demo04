476757a85e97675e55660407ab3e643746f0d3d3
K9042 <K9042@student.jamk.fi>
2017-02-02 15:15:28 +0200

    hissi tehty

---
+++ teh1/Elevator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teh1
+{
+    class Elevator
+    {
+        private readonly int minFloor = 1;
+        private readonly int maxFloor = 5;
+
+        private int floor;
+        public int Floor
+        {
+            get
+            {
+                return floor;
+            }
+            set
+            {
+                if (value <= maxFloor && value >= minFloor)
+                {
+                    floor = value;
+                }
+                else
+                {
+                    Console.WriteLine("Set floor between 1-5!!!!");
+                }
+            }
+        }
+    }
+}
---
+++ teh1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teh1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elevator elevator = new Elevator();
+
+            do
+            {
+                Console.Write("Anna kerros jonne haluat mennä. Valitse 1-5 >");
+                string syote = Console.ReadLine();
+                elevator.Floor = int.Parse(syote);
+
+                Console.WriteLine("Elevator is now in floor " + elevator.Floor);
+            } while (1 == 1);
+        }
+
+    }
+}
