738e8aa2110737c9dfff1552f1fa08cb3ccfd155
K9042 <K9042@student.jamk.fi>
2017-02-02 15:28:04 +0200

    vahvisitn kesken

---
+++ teh2/Amplifier.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teh2
+{
+    class Amplifier
+    {
+        private readonly int minVolume = 0;
+        private readonly int maxVolume = 100;
+
+        private int volume;
+        public int Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                if (value <= maxVolume && value >= minVolume)
+                {
+                    volume = value;
+                }
+                else
+                {
+                    Console.WriteLine("Set volume between 0-100!");
+                }
+            }
+        }
+    }
+}
