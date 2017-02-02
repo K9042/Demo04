7e9367fdd409efce68f68318cd0a03b6948e68a6
K9042 <K9042@student.jamk.fi>
2017-02-02 14:28:21 +0200

    harjoitus

--- Demo04/Program.cs
+++ Demo04/Program.cs
@@ -10,6 +10,25 @@ namespace Demo04
     {
         static void Main(string[] args)
         {
+            Teacher teacher = new Teacher();
+            teacher.FirstName = "Kirsi";
+            teacher.LastName = "Kernel";
+            teacher.Age = 30;
+            teacher.Address = "Piippukatu 2";
+            teacher.PhoneNumber = "010-12345678";
+            teacher.Room = "D444";
+            Console.WriteLine(teacher.ToString());
+
+            Student student = new Student("Matti", "Mallikas", "H9090");
+            student.Address = "Kirkkokatu 10";
+            student.Age = 20;
+            student.PhoneNumber = "040-877878787";
+            Console.WriteLine(student.ToString());
+
+            teacher.TeacherMethod();
+            student.StudentMethod();
+
+
         }
     }
 }
---
+++ Demo04/person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo04
+{
+    class person
+    {
+        public string FirstName { get; set;  }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public person()
+        {
+        }
+
+        public person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public void PersonMethod()
+        {
+            Console.WriteLine("This method belongs to Person!");
+        }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName + " " + Address + " " + Age + " " + PhoneNumber;
+        }
+    }
+}
